8e49f41b915f7be84490eb75794bd2804e06e100
SerhiiCho <serhiicho@protonmail.com>
2019-12-14 21:00:03 +0200

    Added another test to ConvTest.php

--- tests/ConvTest.php
+++ tests/ConvTest.php
@@ -8,6 +8,27 @@ use Serhii\ShortNumber\Conv;
 class ConvTest extends TestCase
 {
     /**
+     * @dataProvider Provider_for_returns_correct_number_between_0_and_899
+     * @test
+     * @param int $number
+     */
+    public function returns_correct_number_between_0_and_899(int $number): void
+    {
+        $this->assertEquals($number, Conv::short($number));
+    }
+
+    public function Provider_for_returns_correct_number_between_0_and_899(): array
+    {
+        $data = [];
+
+        for ($i = 0; $i < 899; $i++) {
+            $data[$i] = [$i];
+        }
+
+        return $data;
+    }
+
+    /**
      * @dataProvider Provider_for_returns_correct_number_between_899_and_899999
      * @test
      * @param int $num_before
@@ -30,22 +51,25 @@ class ConvTest extends TestCase
         return $data;
     }
 
+
     /**
-     * @dataProvider Provider_for_returns_correct_number_between_0_and_899
+     * @dataProvider Provider_for_returns_correct_number_between_899999_and_899999999
      * @test
-     * @param int $number
+     * @param int $num_before
+     * @param int $num_after
      */
-    public function returns_correct_number_between_0_and_899(int $number): void
+    public function returns_correct_number_between_899999_and_899999999(int $num_before, int $num_after): void
     {
-        $this->assertEquals($number, Conv::short($number));
+        $failed_message = "Failed on test #$num_before";
+        $this->assertEquals("{$num_after}M", Conv::short($num_before), $failed_message);
     }
 
-    public function Provider_for_returns_correct_number_between_0_and_899(): array
+    public function Provider_for_returns_correct_number_between_899999_and_899999999(): array
     {
         $data = [];
 
-        for ($i = 0; $i < 899; $i++) {
-            $data[$i] = [$i];
+        for ($i = 900000; $i < 899999999; $i+=100000) {
+            $data[$i] = [$i, number_format(floatval($i / 1000000))];
         }
 
         return $data;
