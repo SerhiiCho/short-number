1f7cc3bb3dbe9d31e667b7d728230922c3a39c71
SerhiiCho <serhiicho@protonmail.com>
2019-12-14 13:00:11 +0200

    Added temporary code to Number.php

--- src/Number.php
+++ src/Number.php
@@ -4,8 +4,47 @@ namespace Serhii\ShortNumber;
 
 class Number
 {
-    public static function shorten(): string
+    /**
+     * Takes number and looks at it, if this number is between 1 thousand and 1 million
+     * function returns this number with "тыс." after number, if its bigger it will
+     * return this number with 'мил.' after.
+     *
+     * @param string $number
+     * @return string
+     */
+    public static function shorten(string $number): string
     {
-        return '';
+        switch (true) {
+            case $number < 900: // 0 - 900
+                $new_number = number_format((float) $number);
+                $suffix = '';
+                break;
+
+            case $number < 900000: // 0.9k-850k
+                $new_number = number_format((float) $number / 1000);
+                $suffix = Lang::trans('thousand');
+                break;
+
+            case $number < 900000000: // 0.9m-850m
+                $new_number = number_format((float) $number / 1000000);
+                $suffix = Lang::trans('million');
+                break;
+
+            case $number < 900000000000: // 0.9b-850b
+                $new_number = number_format((float) $number / 1000000000);
+                $suffix = Lang::trans('billion');
+                break;
+
+            default: // 0.9t+
+                $new_number = number_format((float) $number / 1000000000000);
+                $suffix = Lang::trans('trillion');
+        }
+
+        // Remove unnecessary zeroes after decimal. "1.0" -> "1"; "1.00" -> "1"
+        // Intentionally does not affect partials, eg "1.50" -> "1.50"
+        $dotzero = '.' . str_repeat('0', 1);
+        $new_number = str_replace($dotzero, '', $new_number);
+
+        return "$new_number<br><small>$suffix</small>";
     }
 }
