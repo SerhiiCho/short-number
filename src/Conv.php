568710b88965abc4ec2127830e14d3c7a7f2454c
SerhiiCho <serhiicho@protonmail.com>
2019-12-15 14:25:00 +0200

    Changed method short() on Conv.php

--- src/Conv.php
+++ src/Conv.php
@@ -22,15 +22,15 @@ class Conv
      */
     public static function short(int $num, $options = ''): string
     {
-        self::$options = self::formatOptions(collect($options));
+        self::$options = self::formatOptions($options);
 
         Lang::includeTranslations();
 
         $rules = collect([
-            new Rule(900, 1, '', self::$options),
-            new Rule(900000, 1000, 'thousand', self::$options),
-            new Rule(900000000, 1000000, 'million', self::$options),
-            new Rule(900000000000, 1000000000, 'billion', self::$options),
+            new Rule(1000, 1, '', self::$options),
+            new Rule(1000000, 1000, 'thousand', self::$options),
+            new Rule(1000000000, 1000000, 'million', self::$options),
+            new Rule(1000000000000, 1000000000, 'billion', self::$options),
         ]);
 
          foreach ($rules as $rule) {
@@ -45,11 +45,30 @@ class Conv
     /**
      * Formats dynamic options like 'round 30' etc...
      *
-     * @param \Illuminate\Support\Collection $options
+     * @param array|string $options
      * @return \Illuminate\Support\Collection
      */
-    private static function formatOptions(Collection $options): Collection
+    private static function formatOptions($options): Collection
     {
+        if (is_array($options)) {
+            $options = collect(array_flip($options))->map(function ($_, $key) {
+                return $key;
+            });
+        } else {
+            $options = collect([strval($options) => $options]);
+        }
+
+        $round_options = $options->filter(function ($_, $key) {
+            return !!preg_match('/round (100|[\d]{1,2})/', $key);
+        });
+
+        if ($round_options->isNotEmpty()) {
+            [,$num] = explode(' ', $round_options->first());
+            $num = $num < 50 ? 50 : $num;
+            $num = $num > 100 ? 100 : $num;
+            $options->put('round', intval($num));
+        }
+
         return $options;
     }
 }
